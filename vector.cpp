93a650f767bfe8e349ef9929da8eb735c4d94aa7
YASH162 <devild9912@gmail.com>
2024-10-05 13:23:59 +0530

    add chenages

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Your code here
+    vector<int> v;
+    v.push_back(3);
+    v.push_back(4);
+    for(int i =0;i<2;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
